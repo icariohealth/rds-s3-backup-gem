902bc926ec04db76c1120252a4609385d997eecf
Tamara Temple <tamouse@gmail.com>
2013-03-04 20:04:32 -0600

    add some commentary about syncing version number between gem and chef recipe

--- lib/rds-s3-backup/version.rb
+++ lib/rds-s3-backup/version.rb
@@ -1,6 +1,27 @@
 module Rds
   module S3
     module Backup
+
+      # You have to be aware of this
+      # Unless you wish to dis
+      # The version level here
+      # While updated without fear
+      # May prove to be your downfall
+      # Unless you have the wherewithall
+      # To update your chef recipe
+      # That does the move from rds to s3
+      # And uses this selfsame gem
+      # Upgraded with all it's phlegm
+      # In the gem_package resouce
+      # In that stabled horse
+      # And knifed up to opscode
+      # Before your client leaves it's abode
+      # So happy twiinings can occur
+      # And your humble devops does concur
+      #
+      #  2013-03-04 tamara
+
+
       VERSION = "0.0.5"
     end
   end
